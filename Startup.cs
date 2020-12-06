ff3be49563e5446411fd4253735e47d920b49547
Evan Gannon <flishman13@gmail.com>
2020-12-06 16:57:29 -0500

    The most basic of basics

--- Startup.cs
+++ Startup.cs
@@ -21,20 +21,13 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+         
+           
+               app.Run(async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+           
         }
     }
 }
