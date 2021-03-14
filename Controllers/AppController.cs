471309cef0c392bf71ab90d8e899ec61f9555123
Evan Gannon <flishman13@gmail.com>
2021-03-14 12:36:45 -0400

    Form submission

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -2,21 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
         }
-
+        [HttpGet("contact")]
         public IActionResult Contact()
         {
-            ViewBag.Title = "Contact Us";
+          
             return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("flishman13@gmail.com", model.Subject, $"From:{model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Mail Sent!";
+                ModelState.Clear();
+            }
+            else
+            {
+                //Show error
+            }
+                return View();
         }
 
         public IActionResult About()
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,7 +17,8 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddTransient<IMailService, NullMailService>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,7 +28,7 @@ namespace DutchTreat
                 app.UseDeveloperExceptionPage();
             else
             {
-                //Add Error Page
+                app.UseExceptionHandler("/error");
             }
             app.UseStaticFiles();
             app.UseNodeModules();
